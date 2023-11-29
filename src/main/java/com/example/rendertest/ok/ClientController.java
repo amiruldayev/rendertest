f42b6c3ce8a23a93b34df36773cc7390a75dd28a
amiruldayev <amiruldaeve@gmail.com>
2023-11-29 17:51:39 +0600

    tgv

--- src/main/java/com/example/rendertest/ok/ClientController.java
+++ src/main/java/com/example/rendertest/ok/ClientController.java
@@ -1,12 +1,7 @@
 package com.example.rendertest.ok;
 
-import com.example.rendertest.ClientService;
-import lombok.extern.apachecommons.CommonsLog;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ClientController {
